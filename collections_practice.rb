fe5b33cdbed9d694db0fad2a687acd566091c4ca
Marci Clark <marcig612@gmail.com>
2019-01-26 20:56:15 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,36 @@
+def sort_array_asc(array)
+  array.sort
+  end
+end
 
+sort_array_asc([2, 3, 1])
+ end 
+ 
+ def sort_array_desc(array)
+   array.sort do |a, b|
+    a <=> b
+ end
+end
+
+ sort_array_desc([4, 6, 5])
+ 
+ 
+ def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  end
+end
+
+swap_elements(["dolphins", "whales", "jelly fish"])
+   
+def reverse_array(array)
+  array.reverse
+end
+
+reverse_array(["automobiles", "trains", "planes"])
+  
+  def kesha_maker(array)
+    
+    
+  
+  
+ 
